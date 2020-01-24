1324992e22342c1a42f2d2d68fb37f7d62358a63
Yuka O'oka <oukayuka@ayeate.com>
2020-01-24 13:22:19 +0900

    cut out 6-3.stylelint section

---
+++ 06-lint/03-stylelint/.eslintrc.js
@@ -0,0 +1,70 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:prettier/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+    'prettier/standard',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2019,
+    project: './tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'jsx-a11y',
+    'prettier',
+    'react',
+    'react-hooks',
+  ],
+  root: true,
+  rules: {
+    // NOTE: for react-env.d.ts
+    // https://github.com/facebook/create-react-app/issues/6560
+    'spaced-comment': [
+      'error',
+      'always',
+      { markers: ['/'] },
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        "extensions": ['.jsx', '.tsx'],
+      },
+    ],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+};
---
+++ 06-lint/03-stylelint/.stylelintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-recess-order',
+  ],
+  plugins: [
+    'stylelint-order',
+  ],
+  ignoreFiles: [
+    '**/node_modules/**',
+  ],
+  rules: {
+  },
+};
