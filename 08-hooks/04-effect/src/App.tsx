596c16758329b22f79414c478c1677b317a5c93c
Yuka O'oka <oukayuka@ayeate.com>
2020-02-14 19:37:10 +0900

    add section 8-4 effect hook

---
+++ 08-hooks/04-effect/src/App.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react';
+import Timer from './Timer';
+import './App.css';
+
+const App: FC = () => (
+  <div className="container">
+    <header>
+      <h1>タイマー</h1>
+    </header>
+    <Timer limit={60} />
+  </div>
+);
+
+export default App;
