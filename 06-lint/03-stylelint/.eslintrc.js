983c0a285666f8a40bb6dc049330626830db18bb
Yuka O'oka <oukayuka@ayeate.com>
2020-12-14 20:14:10 +0900

    finish 6-3 stylelint

--- 06-lint/03-stylelint/.eslintrc.js
+++ 06-lint/03-stylelint/.eslintrc.js
@@ -10,35 +10,33 @@ module.exports = {
     'plugin:import/errors',
     'plugin:import/warnings',
     'plugin:import/typescript',
-    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:prettier/recommended',
     'prettier',
     'prettier/@typescript-eslint',
     'prettier/react',
-    'prettier/standard',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 2020,
+    ecmaVersion: 12,
     project: './tsconfig.eslint.json',
     sourceType: 'module',
     tsconfigRootDir: __dirname,
   },
   plugins: [
-    '@typescript-eslint',
-    'import',
-    'jsx-a11y',
-    'prettier',
     'react',
-    'react-hooks',
+    '@typescript-eslint',
   ],
   root: true,
   rules: {
+    // occur error in `import React from 'react'` with react-scripts 4.0.1
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': [
+      'error',
+    ],
     'lines-between-class-members': [
       'error',
       'always',
@@ -46,9 +44,12 @@ module.exports = {
         exceptAfterSingleLine: true,
       },
     ],
-    // should be rewritten as `['error', { allowAsStatement: true }]` in ESLint 7 or later
-    // SEE: https://github.com/typescript-eslint/typescript-eslint/issues/1184
-    'no-void': 'off',
+    'no-void': [
+      'error',
+      {
+        allowAsStatement: true,
+      },
+    ],
     'padding-line-between-statements': [
       'error',
       {
@@ -91,6 +92,7 @@ module.exports = {
         explicitSpread: 'ignore',
       },
     ],
+    'react/react-in-jsx-scope': 'off',
   },
   overrides: [
     {
--- 06-lint/03-stylelint/src/App.tsx
+++ 06-lint/03-stylelint/src/App.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
+import { FC } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => (
+const App: FC = () => (
   <div className="App">
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
       <p>
-        Edit
-        <code>src/App.tsx</code>
-        and save to reload.
+        Edit <code>src/App.tsx</code> and save to reload.
       </p>
       <a
         className="App-link"
