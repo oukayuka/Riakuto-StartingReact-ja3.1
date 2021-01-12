c3237491f87336d5ee571ea789e44a894f8e6cae
Yuka O'oka <oukayuka@ayeate.com>
2021-01-12 12:49:22 +0900

    update .eslintrc.js

--- 06-lint/01-eslint/.eslintrc.js
+++ 06-lint/01-eslint/.eslintrc.js
@@ -1,9 +1,10 @@
 module.exports = {
   env: {
     browser: true,
-    es2020: true,
+    es2021: true,
   },
   extends: [
+    'plugin:react/recommended',
     'airbnb',
     'airbnb/hooks',
     'plugin:import/errors',
