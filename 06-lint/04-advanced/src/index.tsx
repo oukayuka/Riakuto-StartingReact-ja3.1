640bc2e2d853c7274fe2fb5a58da7b70d16ed2e5
Yuka O'oka <oukayuka@ayeate.com>
2021-03-31 09:57:30 +0900

    migrate husky to simple-git-hooks

--- 06-lint/04-advanced/src/index.tsx
+++ 06-lint/04-advanced/src/index.tsx
@@ -8,7 +8,7 @@ ReactDOM.render(
   <StrictMode>
     <App />
   </StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
